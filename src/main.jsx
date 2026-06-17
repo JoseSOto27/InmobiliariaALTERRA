7bfbf670e46cdf5b7ba1eb8b1bd333bedf4654a0
JoseSoto <snow.ball2709@aol.com>
2026-06-17 13:04:15 -0600

    feat: configuraciones tailwind arreglada_v2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'leaflet/dist/leaflet.css' // 👈 ¡Agregado aquí para que no rompa Tailwind en producción!
 import './index.css'
 import App from './App.jsx'
 
