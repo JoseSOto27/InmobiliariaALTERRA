7b865f14777c747697a6d4810285a38d9ff28b85
JoseSoto <snow.ball2709@aol.com>
2026-06-17 13:08:15 -0600

    feat: configuraciones tailwind arreglada_v3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'leaflet/dist/leaflet.css' // 👈 ¡Agregado aquí para que no rompa Tailwind en producción!
-import './index.css'
+import './index.css' // 👈 Solo dejamos el CSS limpio de Tailwind
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
